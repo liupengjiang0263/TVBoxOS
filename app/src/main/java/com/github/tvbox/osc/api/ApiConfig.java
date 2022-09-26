23b039f8813887da6c9af7680c5fd8627a67bd5f
liupengjiang0263 <108129478+liupengjiang0263@users.noreply.github.com>
2022-09-26 16:04:29 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -80,7 +80,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://gitea.com/Yoursmile/TVBox/raw/branch/main/XC.json");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://agit.ai/Yoursmile7/TVBox/raw/branch/master/XC.json");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
